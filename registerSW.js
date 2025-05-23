ecd04148aacf87cdf701ad75419b1e73711b6540
alexander <dreamtheater@list.ru>
2025-05-23 09:39:33 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/lastochki/sw.js', { scope: '/lastochki/' })})}
